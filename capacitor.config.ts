d710a7c1ad9b090df7bf889a4f0757a529fe562b
Topasm <dhkdwk0215@naver.com>
2026-03-13 23:54:54 +0900

    Use HTTP Android scheme for Tailscale API access

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,7 +5,7 @@ const config: CapacitorConfig = {
   appName: 'ClawChat',
   webDir: 'dist',
   server: {
-    androidScheme: 'https',
+    androidScheme: 'http',
   },
   plugins: {
     Keyboard: {
