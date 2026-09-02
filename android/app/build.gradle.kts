712a5eacd2f19929accaaecdb97d3478b3898712
Topasm <dhkdwk0215@naver.com>
2026-09-02 20:13:09 +0900

    fix(macos): allow HTTP workspace connections

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         applicationId = "com.clawchat.android"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionName = "1.4.11"
+        versionName = "1.4.12"
         versionCode = androidVersionCode(versionName!!)
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
