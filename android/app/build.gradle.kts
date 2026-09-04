f423cc5afd39390e7e5183e969147e3931efcc7d
Topasm <dhkdwk0215@naver.com>
2026-09-04 20:50:49 +0900

    chore(release): bump version to 1.4.19

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         applicationId = "com.clawchat.android"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionName = "1.4.18"
+        versionName = "1.4.19"
         versionCode = androidVersionCode(versionName!!)
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
